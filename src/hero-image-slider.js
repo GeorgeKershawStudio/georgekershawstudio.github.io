88bb2634cb7bb320cf51d4547528e8a4a807335e
GeorgeKershawStudio <georgekershawstudio@gmail.com>
2024-07-15 20:17:48 +0100

    first commit

---
+++ src/hero-image-slider.js
@@ -0,0 +1,53 @@
+function duplicateContainers() {
+    const originalContainer = document.getElementById('heroSlider').outerHTML;
+    const duplicatedContainers = document.getElementById('heroSection');
+    duplicatedContainers.innerHTML = '';
+
+    const screenWidth = window.innerWidth;
+    const numberOfContainers = Math.floor(screenWidth / 150);
+
+    const imageSets = [
+        ["/IrnBru-Leith-Agency-Design.jpg", 
+            "/Portfolio-Template-Img.jpg", 
+            "/IrnBru-Leith-Agency-Design.jpg", 
+            "/Portfolio-Template-Img.jpg", 
+            "/IrnBru-Leith-Agency-Design.jpg", 
+            "/Portfolio-Template-Img.jpg", ],
+        ["/IrnBru-Leith-Agency-Design.jpg", 
+            "/Portfolio-Template-Img.jpg", 
+            "/IrnBru-Leith-Agency-Design.jpg", 
+            "/Portfolio-Template-Img.jpg", 
+            "/IrnBru-Leith-Agency-Design.jpg", 
+            "/Portfolio-Template-Img.jpg", ]
+    ];
+
+    for (let i = 0; i < numberOfContainers; i++) {
+        const newContainerWrapper = document.createElement('div');
+        newContainerWrapper.innerHTML = originalContainer;
+        const newContainer = newContainerWrapper.firstElementChild;
+
+        newContainer.classList.add('hero-slider');
+
+        const heroImages = newContainer.querySelector('.hero-images');
+        if (i % 4 === 1) {
+            heroImages.classList.add('hero-reverse-anim');
+        } else if (i % 4 === 2) {
+            heroImages.classList.add('hero-offset-anim');
+        } else if (i % 4 === 3) {
+            heroImages.classList.add('hero-reverse-offset-anim');
+        }
+
+        const imageSet = imageSets[i % imageSets.length];
+        
+        const imageMaskers = heroImages.querySelectorAll('.image-masker img');
+        imageMaskers.forEach((img, index) => {
+            img.src = imageSet[index % imageSet.length];
+        });
+
+        duplicatedContainers.appendChild(newContainer);
+    }
+}
+
+duplicateContainers();
+
+window.addEventListener('resize', duplicateContainers);
