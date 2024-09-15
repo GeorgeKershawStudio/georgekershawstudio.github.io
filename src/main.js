f60feb060ddf9eb57eda66b7d06e4720c29d4ceb
GeorgeKershawStudio <georgekershawstudio@gmail.com>
2024-09-15 17:30:49 +0100

    update

--- src/main.js
+++ src/main.js
@@ -3,5 +3,6 @@ import '../styles/style.css'
 import '../styles/hero-image-slider.css'
 import '../styles/utilities.css'
 import '../styles/text-styles.css'
+import '../styles/portfolio-section.css'
 
 import './hero-image-slider.js'
