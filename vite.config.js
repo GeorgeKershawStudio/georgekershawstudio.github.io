01e638655777c9684f76e7cc8b88ee521b4a40a2
GeorgeKershawStudio <georgekershawstudio@gmail.com>
2026-05-11 15:23:28 +0100

    please22

--- vite.config.js
+++ vite.config.js
@@ -6,3 +6,21 @@ export default {
     }
   }
 }
+import { defineConfig } from 'vite'
+import { resolve } from 'path'
+
+export default defineConfig({
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        istd: resolve(__dirname, 'istd.html'),
+        edin: resolve(__dirname, 'edin-council.html'),
+        octibook: resolve(__dirname, 'octibook.html'),
+        birmingham2022: resolve(__dirname, 'birmingham2022.html'),
+        patd: resolve(__dirname, 'patd.html'),
+        // one line per HTML file
+      }
+    }
+  }
+})
